a8e4bef650b51d9863f8779d6e1c073677f65eae
Loïc Donot <loicdonot@imacdeloicdonot.home>
2016-08-20 18:44:23 +0200

    Plus de flux.

--- index.php
+++ index.php
@@ -4,8 +4,13 @@ $radios = array(
 	array("France Info","http://audio.scdn.arkena.com/11006/franceinfo-midfi128.mp3"),
 	array("France Culture","http://audio.scdn.arkena.com/11010/franceculture-midfi128.mp3"),
 	array("France Musique","http://audio.scdn.arkena.com/11012/francemusique-midfi128.mp3"),
+	array("Mouv'","http://audio.scdn.arkena.com/11014/mouv-midfi128.mp3"),
 	array("FIP (National)","http://audio.scdn.arkena.com/11016/fip-midfi128.mp3"),
-	array("Mouv'","http://audio.scdn.arkena.com/11014/mouv-midfi128.mp3")
+	array("FIP (autour du Rock)","http://webradiofip1.scdn.arkena.com/webradiofip1.mp3"),
+	array("FIP (autour du Jazz)","http://webradiofip2.scdn.arkena.com/webradiofip2.mp3"),
+	array("FIP (autour du Groove)","http://webradiofip3.scdn.arkena.com/webradiofip3.mp3"),
+	array("FIP (autour du Monde)","http://webradiofip4.scdn.arkena.com/webradiofip4.mp3"),
+	array("FIP (tout Nouveau)","http://webradiofip5.scdn.arkena.com/webradiofip5.mp3"),
+	array("FIP (il y a 45 ans)","http://webradiofip6.scdn.arkena.com/webradiofip6.mp3")
 );
 include("template.php");
- 
